15f2e49182343964be87c920c9567e43c5e3c399
Gabriel Gavinowich <ggavinowich@gmail.com>
2011-04-03 14:40:04 -0300

    No graba el test del s52 :( Probe de todo. Copiar otros programadores Ir paso a paso Etc

--- codigo/juntada/test8253.c
+++ codigo/juntada/test8253.c
@@ -1,3 +1,7 @@
+//#define TEST_8253
+
+#ifdef TEST_8253
+
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include "def.h"
@@ -7,6 +11,7 @@
 #include "spi.h"
 
 
+
 byte_t programEnable_Cmm[] = {0xAC,0x53,0x00,0x00};
 byte_t chipErase_Cmm[]     = {0xAC,0x80,0x00,0x00};
 byte_t writeByte_Cmm[]     = {0x40,0x00,0x00,0x00};
@@ -14,7 +19,12 @@ byte_t readByte_Cmm[]      = {0x20,0x00,0x00,0x00};
 byte_t writePage_Cmm[]     = {0x50,0x00,0x00,0x00};
 byte_t readPage_Cmm[]      = {0x03,0x00,0x00,0x00};
 
-byte_t led_05s_program[] = {
+#define TEST_LED_1_SEG
+//#define TEST_LED_05_SEG
+
+#ifdef TEST_LED_05_SEG
+
+byte_t led_program[] = {
 0xB2,
 0x80,
 0x11,
@@ -62,8 +72,9 @@ byte_t led_05s_program[] = {
 0x22,
 };
 
+#elif defined(TEST_LED_1_SEG)
 
-byte_t led_1s_program[] = {
+byte_t led_program[] = {
 0xB2,
 0x80,
 0x11,
@@ -111,6 +122,8 @@ byte_t led_1s_program[] = {
 0x22,
 };
 
+#else error("NO CODE TO PROGRAM")
+#endif
 
 void test8253_normal_write()
 {
@@ -118,9 +131,9 @@ void test8253_normal_write()
 
 }
 
-/* Sends one byte over SPI.
-   Returns the data in.
-*/
+// Sends one byte over SPI.
+//   Returns the data in.
+
 
 static byte_t spiOneByte(byte_t dataOut)
 {
@@ -200,24 +213,33 @@ void WriteProgramByByte()
     for (i = 0; i < addressTotal; i++,address++)
     {
         writeByte_Cmm[2] = address;
-        writeByte_Cmm[3] = led_1s_program[i];        
+        writeByte_Cmm[3] = led_program[i];        
         spiOneCommand(writeByte_Cmm,result,4);
         WaitDateWriteCompletion(writeByte_Cmm[3], address, 4);
     }
 }
 
+//El byte anterior me permite asegurar que no perdi la sincronia. En principio no es necesario.
+//Tampoco se que se puede hacer en caso de perderla.
 void WriteProgramByPage()
 {
-    byte_t address = 0 ,i ;
+    byte_t address = 0 ,i, dataInAnterior, dataOutAnterior ;
     byte_t addressTotal = 45;
     byte_t result[4];
 
     spiOneByte(writePage_Cmm[0]);
     spiOneByte(writePage_Cmm[1]);
     spiOneByte(writePage_Cmm[2]);
+    dataOutAnterior = writePage_Cmm[2];
     for (i = 0; i < addressTotal; i++,address++)
     {
-        spiOneByte(led_05s_program[i]);        
+        dataInAnterior = spiOneByte(led_program[i]);        
+        if (dataInAnterior != dataOutAnterior)
+        {   
+            return;// Si perdio la sincronia no se que hacer, quiza habilitar una espera y seguir...
+        }
+        else 
+            dataOutAnterior = led_program[i];
     }
     
     //Completo la pagina con 0x00
@@ -226,8 +248,7 @@ void WriteProgramByPage()
         spiOneByte(0x00);
     }
 
-    WaitDateWriteCompletion(led_05s_program[0], 0x00, 4);    //Chequeeo la primer posicion
-
+    WaitDateWriteCompletion(led_program[0], 0x00, 4);    //Chequeeo la primer posicion
 }
 
 //Inicio, activo el buffer, subo el reset, prendo el LED y configuro el MOSI como salida.
@@ -267,3 +288,4 @@ void ProgramLed()
     ClosePrograming();
 }
 
+#endif
